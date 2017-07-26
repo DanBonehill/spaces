a6381a18cd08c7849d8ee46dcf4ea232a6cec814
DanBonehill <DanBonehill@me.com>
2017-07-26 18:14:24 +0530

    Add to sign_up_spec. 1 test currently failing due to FactoryGirl issue.

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature "A member signs up" do
+  let(:member) {FactoryGirl.create(:member)}
+
+  before do
+    visit root_path
+    click_link "Sign up"
+  end
+
+  scenario "with valid credentials" do
+    fill_in "member[email]", with: member.email
+    fill_in "member[password]", with: member.password
+    fill_in "member[password_confirmation]", with: member.password
+
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+
+  scenario "without email credentials" do
+    fill_in "member[email]", with: " "
+    fill_in "member[password]", with: member.password
+    fill_in "member[password_confirmation]", with: member.password
+
+    click_button "Sign up"
+
+    expect(page).to have_content("Email can't be blank")
+  end
+
+  scenario "without password credentials" do
+    fill_in "member[email]", with: member.email
+    fill_in "member[password]", with: " "
+    fill_in "member[password_confirmation]", with: " "
+
+    click_button "Sign up"
+
+    expect(page).to have_content("Password can't be blank")
+  end
+end
