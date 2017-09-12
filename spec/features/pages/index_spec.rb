c4a5f15c4e3b0a0c94cf85590c4621d382f04b90
DanBonehill <DanBonehill@me.com>
2017-09-12 20:43:33 +0100

    Add styling to app

--- spec/features/pages/index_spec.rb
+++ spec/features/pages/index_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "visting the homepage" do
   scenario "the visitor see the welcome text" do
     visit root_path
-    expect(page).to have_content("Hello World")
+    expect(page).to have_selector("h1", text: "Welcome to Spaces")
   end
 end
