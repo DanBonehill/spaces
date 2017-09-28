35e06bd795281dfa9de4da12031ed85b17bfc1b2
DanBonehill <DanBonehill@me.com>
2017-09-28 07:19:02 +0100

    Edit styling & update homepage
    Edit styling of Devise shared links
    Update homepage link_to with url

--- spec/features/pages/index_spec.rb
+++ spec/features/pages/index_spec.rb
@@ -5,4 +5,9 @@ feature "visting the homepage" do
     visit root_path
     expect(page).to have_selector("h1", text: "Welcome to Spaces")
   end
+  scenario "the visitor goes to sign up" do
+    visit root_path
+    click_link "Become a member"
+    expect(page).to have_selector("h2", text: "Sign up")
+  end
 end
