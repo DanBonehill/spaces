a171a847f816f6861a60170e604de0c5ee42320a
DanBonehill <DanBonehill@me.com>
2017-10-11 20:24:17 +0100

    Update profiles factory to prevent intermittent test failures

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :profile do
-    first_name { FFaker::Name.first_name }
+    first_name { FFaker::Lorem.characters(character_count = rand(3..19)) }
     last_name { FFaker::Name.last_name }
     contact_number { FFaker::PhoneNumber.phone_number[1...19] }
     member
